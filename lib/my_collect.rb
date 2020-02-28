28cfa6406a8a1b7bdd3cb1526efcd6812ed5c5ba
Daniel Kim <daniel.kim52@gmail.com>
2020-02-28 02:18:27 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(students)
   i = 0
   first_name = []
   while i < students.length
-    first_name << yield(students[i]).split(" ").first
+    first_name << yield(students[i])
     i += 1
   end
   first_name
