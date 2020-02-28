499cc4a33a766d956a3c285faabb21b716102672
Daniel Kim <daniel.kim52@gmail.com>
2020-02-28 02:15:17 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,13 @@
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+def my_collect(students)
+  i = 0
+  first_name = []
+  while i < students.length
+    first_name << yield(students[i]).split(" ").first
+    i += 1
+  end
+  first_name
+end
+
 my_collect(array) do |name|
   name.split(" ").first
 end
